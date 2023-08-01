76c09b80e1633ca0d38934156ce61a645cc0550d
Noomuu <141158389+Noomuu@users.noreply.github.com>
2023-08-01 23:31:00 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const typedTextSpan = document.querySelector(".typed-text");
 const cursorSpan = document.querySelector(".cursor");
 
-const textArray = ["OWT Player", "Alessatax is my boy", "Fabatax#6403"];
+const textArray = ["Hardstuck Bronze", "Proud botfrag Reyna", "Searching for pocket sage :)"];
 const typingDelay = 200;
 const erasingDelay = 100;
 const newTextDelay = 500; // Delay between current and next text
